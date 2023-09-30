42c97b4de87a97c5a96848057ddfb5b3cdd2da6f
joe <31982488+Youssef-Shehata@users.noreply.github.com>
2023-09-30 22:57:59 +0300

    dynamically populated database for testing purposes started working on controllers to create searching functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,34 @@
 
+const Product = require('../models/product')
+const companies = ['ikea', 'liddy', 'caressa', 'marcos']
 
 const getAllProductsStatic = async(req,res) =>{
-    
+    const products = await Product.find({company: 'ikea'})
+
     res.status(200)
-        .json({msg:"testing products router"})
+        .json({products,nhits : products.length})
 }
 
 const getAllProducts = async(req,res) =>{
+    const {featured , company ,  name} = req.query
+    const queryObject = {}
+    if(featured ){
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+
+    if(company && companies.includes(company)){
+        queryObject.company = company
+    }
+
+
+    if(name){
+        queryObject.name = {$regex: name , $options: 'i'}
+    }
+    const products = await Product.find(queryObject)
+    console.log(queryObject.featured)
     res.status(200)
-        .json({msg:" products route"})
+        .json({products,nHits: products.length})
 }
 
 module.exports = {
--- models/product.js
+++ models/product.js
@@ -34,3 +34,5 @@ const productSchema = new mongoose.Schema({
     }
 
 })
+
+module.exports = mongoose.model("Product", productSchema)
