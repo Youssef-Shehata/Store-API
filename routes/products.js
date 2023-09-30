27ae20567757f99e299e2c3ef76c5c9f9b771a20
joe <31982488+Youssef-Shehata@users.noreply.github.com>
2023-09-30 21:48:47 +0300

    fixed a bug

--- routes/products.js
+++ routes/products.js
@@ -8,5 +8,4 @@ const { getAllProducts, getAllProductsStatic} = require('../controllers/products
 
 router.route('/').get(getAllProducts)
 router.route('/static').get(getAllProductsStatic)
-
 module.exports = router
