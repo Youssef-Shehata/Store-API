707c3249dbce9166e2376ed1a5a400be9bca1226
joe <31982488+Youssef-Shehata@users.noreply.github.com>
2023-10-01 21:19:46 +0300

    added searching filters on all attributes of products

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,18 @@
 const Product = require('../models/product')
 const companies = ['ikea', 'liddy', 'caressa', 'marcos']
 
-const getAllProductsStatic = async(req,res) =>{
-    const products = await Product.find({company: 'ikea'})
 
-    res.status(200)
-        .json({products,nhits : products.length})
-}
+//static testing::>
+
+// const getAllProductsStatic = async(req,res) =>{
+//     const products = await Product.find({company: 'ikea'})
+
+//     res.status(200)
+//         .json({products,nhits : products.length})
+// }
 
 const getAllProducts = async(req,res) =>{
-    const {featured , company ,  name} = req.query
+    const {featured , company ,  name, sort,fields ,numericFilters } = req.query
     const queryObject = {}
     if(featured ){
         queryObject.featured = featured === 'true' ? true : false
@@ -25,13 +28,67 @@ const getAllProducts = async(req,res) =>{
     if(name){
         queryObject.name = {$regex: name , $options: 'i'}
     }
-    const products = await Product.find(queryObject)
-    console.log(queryObject.featured)
+
+
+
+
+    let result =  Product.find(queryObject)
+
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        console.log(sortList)
+
+        result = result.sort(sortList)
+    }else{
+        result.sort('createdAt')
+    }
+
+
+    if(fields){
+        const fieldList  = fields.split(',').join(' ')
+        console.log(fieldList)
+
+        result = result.select(fieldList)
+    }
+
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) ||4
+    let skip = (page-1)* limit 
+    if(numericFilters){
+    
+        const operatorMap= {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+        const options = ['price','rating']
+        filters = filters.split(',').forEach( item =>{
+            
+            const [field, operator,value] = item.split('-')
+            if(options.includes(field)){
+            queryObject[field] = {[operator] : Number(value)}
+            }
+            
+        })
+        
+        result  = result.find(queryObject)
+    }
+
+    const products = await result
+
+
+    result = result.skip(skip).limit(limit)
     res.status(200)
         .json({products,nHits: products.length})
+
 }
 
 module.exports = {
-    getAllProducts,
-    getAllProductsStatic
+    getAllProducts
 }
